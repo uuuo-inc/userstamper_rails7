4ef6c5889439cb5f5144f2a96cff32d9b92d51a8
kt11022020 <60920898+kt11022020@users.noreply.github.com>
2022-06-16 16:09:54 +0900

    ControllerConcernのカスタマイズ (#1)
    * ControllerConcernのカスタマイズ
    
    * printf ログ削除した
    
    * カスタマイズした箇所にコメントを入れた

--- lib/userstamper/controller_concern.rb
+++ lib/userstamper/controller_concern.rb
@@ -7,11 +7,20 @@
 module Userstamper::ControllerConcern
   extend ActiveSupport::Concern
 
-  included do
-    around_action :with_stamper
+  # RE-IMPLED <<<
+  # The `around_action`, `with_stamper` and `set_stamping_user` are added in our controller which
+  # includes this concern module. Instead of `current_user`, the `set_stamping_user` can be given
+  # a user roled as stamper.
+  def set_stamping_user(user)
+    @stamping_user = user
   end
 
-  private
+  # included do
+  #  around_action :with_stamper
+  # end
+
+  # private
+  # RE-IMPLED >>>
 
   # This {#with_stamper} method sets the stamper for the duration of the action. This ensures
   # that exceptions raised within the controller action would properly restore the previous stamper.
@@ -24,6 +33,8 @@ module Userstamper::ControllerConcern
     reset_stamper
   end
 
+  private
+
   # The {#set_stamper} method as implemented here assumes a couple of things. First, that you are
   # using a +User+ model as the stamper and second that your controller has a +current_user+
   # method that contains the currently logged in stamper. If either of these are not the case in
@@ -31,7 +42,7 @@ module Userstamper::ControllerConcern
   # private section of your +ApplicationController+
   def set_stamper
     @_userstamp_stamper = Userstamper.config.default_stamper_class.stamper
-    Userstamper.config.default_stamper_class.stamper = current_user
+    Userstamper.config.default_stamper_class.stamper = @stamping_user
   end
 
   # The {#reset_stamper} method as implemented here assumes that a +User+ model is being used as
